9c4bb3ce25d0a1e4a2c31e9bde7b083d2e8a7e33
Nic Dawson <nic.rednetwork@gmail.com>
2024-10-17 00:01:54 -0400

    Initial file organization and pseudocode

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,8 +1,72 @@
 // dijsktras.cpp
 
-// Main Execution
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
 
-int main(int argc, char *argv[]) {
-    return 0;
+using namespace std;
+
+
+// TODO: this code is an augmented form of BFS, which should be able to be modified into djikstras
+// Code copied from challenge 4 - Returns true if there is a valid path from src to dest
+bool isPath(unordered_map<char, vector<char>> &adjMap, const char &src, const char &dest) {
+  vector<char> visited;
+  queue<char> queue;
+  queue.push(src);
+
+  while(queue.size() != 0) {
+    char curr = queue.front(); // Pop next node off queue
+    queue.pop();
+
+    visited.push_back(curr);      // Mark node as visited
+    if(curr == dest) return true; // Return true if dest if found
+
+    // Push curr's adjacent nodes to queue if they haven't been already visited
+    unordered_map<char, vector<char>>::iterator currPos = adjMap.find(curr);
+    for(unsigned i = 0; i < currPos->second.size(); i++) { // For every adjacent node...
+      bool isVisited = false;
+      for(unsigned j = 0; j < visited.size(); j++) { // Check against every node in visited vec
+        if(currPos->second[i] == visited[j]) isVisited = true; // Don't push if they are equal
+      }
+      if(isVisited == false) queue.push(currPos->second[i]);
+    }  
+  }
+
+  return false; // All possible nodes visited, dest is unreachable
 }
 
+
+// Main Execution
+int main(int argc, char *argv[]) {
+    /* Input handling */
+    // nTiles - number of different tiles on board
+    // tileChar1 tileCost1 (repeated nTiles times)
+    // mapX mapY - size of board
+    // Board matrix itself
+    // startRow startCol - coordinate where pathfinding will start
+    // endRow endCol - coordinate where pathfinding will end
+
+    /* Perform dijkstras algorithm and get optimal path from the start coord to the end coord */
+    /*
+    From notes: https://web.eecs.utk.edu/~semrich/cs302-24/notes/11+12/
+    For all nodes, set their backedges to NULL, their distances to -1, and their "visited" field to be false.
+    Set node 0's distance to zero and put it on the multimap.
+    While multimap isn't empty...
+        Remove a node n from the front of the multimap and set its visited field to true.
+        For each edge e from n to n2 such that n2 has not been visited. Let d be n's distance plus the weight of edge e. If n2's distance is -1, or if d is less than node n2's current distance:
+            If n2 was in the multimap, remove it.
+            Set n2's distance to d.
+            Set n2's backedge to e.
+            Insert n2 into the multimap, keyed on distance.
+    */
+
+
+    /* Output handling */
+    // Total cost
+    // Tiles travelled
+    return 0;
+}
---
+++ src/generate_map.cpp
@@ -0,0 +1,31 @@
+// generate_map.cpp
+
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+/*
+Tile costs:
+f 3
+g 1
+G 2
+h 4
+m 7
+r 5
+*/
+
+
+// Main Execution - given N (as a command argument), generates a .txt file containing an NxN board with random tiles
+int main(int argc, char *argv[]) {
+    // Create and open a new file (name it N.txt?)
+
+    // Loop n times (i loop)...
+        // Loop n times (j loop)...
+            // fout a random tile char
+        // fout an endline char
+    // Close fout
+    return 0;
+}
