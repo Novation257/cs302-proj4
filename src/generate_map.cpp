7cd85b2db55076bb90bc36f947577992396f4b98
Novation257 <91281416+Novation257@users.noreply.github.com>
2024-10-21 17:36:57 -0400

    Final revision(?) of generate_map.cpp

--- src/generate_map.cpp
+++ src/generate_map.cpp
@@ -29,6 +29,7 @@ char randTile() {
         case 4: return 'm';
         case 5: return 'r';   
     }
+    return 'X';
 }
 
 // Main Execution - given N (as a command argument), generates a .txt file containing an NxN board with random tiles
@@ -40,8 +41,8 @@ int main(int argc, char *argv[]) {
     }
 
     // Create new file with name argv[1] and convert argv[0] to an int
-    ofstream fout(argv[1]);
-    int n = stoi(argv[0]);
+    ofstream fout(argv[2]);
+    int n = stoi(argv[1]);
 
     for(int i = 0; i < n; i++) { // Create n lines...
         for(int j = 0; j < n; j++) { // ... with n tiles in each line
