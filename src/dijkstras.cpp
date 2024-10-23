cc1e94d31d257e5cb602b3f83fc131bfc7ea614f
Nic Dawson <nic.rednetwork@gmail.com>
2024-10-23 13:57:10 -0400

    Completed dijkstras.cpp and code cleanup

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -11,11 +11,6 @@
 
 using namespace std;
 
-// Returns the closest bound if val is out of bounds; otherwise returns val itself
-int clamp(int val, int maxVal) {
-  return min(max(val, 0), maxVal);
-}
-
 // Changes a cell's expression from coordinates to the ID format
 int toID(vector<int> cellCoords, int n) {
   return (cellCoords[0]*n + cellCoords[1]);
@@ -26,14 +21,9 @@ vector<int> toCoords(int cellID, int n) {
   return {cellID/n, cellID%n};
 }
 
-// Converts an int to a char (0=a, 1=b, etc.)
-char itoc(int num) {
-  return (num+65);
-}
-
 // COMPLETED?
 // Returns a 3d matrix with containing the optimal paths to every coord [X][Y][step]
-vector<vector<vector<int>>> djikstrasAlgorithm(vector<vector<int>> board, const int &srcID, const int &destID) {
+vector<vector<vector<int>>> djikstrasAlgorithm(vector<vector<int>> board, const int &srcID) {
   vector<vector<bool>> visited;       // Holds true when coord has been visited
   vector<vector<vector<int>>> paths;  // Each coord holds the cell IDs taken in the optimal path
   vector<vector<int>> distances;      // Distances from src
@@ -75,19 +65,12 @@ vector<vector<vector<int>>> djikstrasAlgorithm(vector<vector<int>> board, const 
       if(currY+1 < n  && visited[currX][currY+1] == false) edgesCoords.push_back({ currX  ,      currY+1 }); // Right
       if(currY-1 >= 0 && visited[currX][currY-1] == false) edgesCoords.push_back({ currX  ,      currY-1 }); // Left
 
-      // cout << "Node " << currX << " " << currY << endl; // DEBUG - print curr
-
       // For each of curr's neighbors (up, down, left, right)...
       for(unsigned i = 0; i < edgesCoords.size(); i++) {
         int nextX = edgesCoords[i][0];
         int nextY = edgesCoords[i][1];
         if(visited[nextX][nextY] == false) { // If the node hasn't been visited already...
           if(distances[currX][currY] + board[currX][currY] < distances[nextX][nextY]) { // If current path is "cheaper"...
-
-            // // DEBUG - printing changes to path
-            // cout << "At " << nextX << " " << nextY << " from " << currX << " " << currY << "... ";
-            // cout << "weight " << distances[currX][currY] + board[currX][currY] << " is less than " << distances[nextX][nextY] << endl;
-
             // Update distance (current distance from start + current node's weight)
             distances[nextX][nextY] = distances[currX][currY] + board[currX][currY];
 
@@ -142,26 +125,25 @@ int main(int argc, char *argv[]) {
   int endRow, endCol;
   cin >> endRow >> endCol;
 
-  // Convert start and end positions to ID format
+  // Convert start coordinate to ID format
   int startID = toID({startRow, startCol}, mapX);
-  int endID = toID({endRow, endCol}, mapX);
 
   /* Perform dijkstras algorithm and get optimal path from the start coord to the end coord */
-  vector<vector<vector<int>>> optimalPaths = djikstrasAlgorithm(boardConverted, startID, endID);
+  vector<vector<vector<int>>> optimalPaths = djikstrasAlgorithm(boardConverted, startID);
 
   /* Output handling */
   // Total cost
   int totalCost = 0;
-  for(unsigned i = 0; i < optimalPaths[endCol][endRow].size(); i++) {
-    vector<int> coords = toCoords(optimalPaths[endCol][endRow][i], mapX);
+  for(unsigned i = 0; i < optimalPaths[endRow][endCol].size(); i++) {
+    vector<int> coords = toCoords(optimalPaths[endRow][endCol][i], mapX);
 
     totalCost += boardConverted[coords[0]][coords[1]];
   }
   cout << totalCost << endl;
 
   // Path travelled
-  for(unsigned i = 0; i < optimalPaths[endCol][endRow].size(); i++) {
-    vector<int> coords = toCoords(optimalPaths[endCol][endRow][i], mapX);
+  for(unsigned i = 0; i < optimalPaths[endRow][endCol].size(); i++) {
+    vector<int> coords = toCoords(optimalPaths[endRow][endCol][i], mapX);
     cout << coords[0] << " " << coords[1] << endl;
   }
   cout << endRow << " " << endCol << endl;
