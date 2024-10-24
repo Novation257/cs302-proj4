50c4432de347ae113886cc757b11edee6f385395
Nic Dawson <nic.rednetwork@gmail.com>
2024-10-23 21:20:14 -0400

    Rewrote generate_map.cpp to match formatting

--- src/generate_map.cpp
+++ src/generate_map.cpp
@@ -40,16 +40,38 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Seed rand with time
+    srand(time(0));
+
     // Create new file with name argv[1] and convert argv[0] to an int
     ofstream fout(argv[2]);
     int n = stoi(argv[1]);
 
+    // Fout tile types
+    fout << "6" << endl;
+    fout << "f 3" << endl;
+    fout << "g 1" << endl;
+    fout << "G 2" << endl;
+    fout << "h 4" << endl;
+    fout << "m 7" << endl;
+    fout << "r 5" << endl;
+
+    // Fout board size
+    fout << n << " " << n << endl;
+
+    // Fout randomised board
     for(int i = 0; i < n; i++) { // Create n lines...
         for(int j = 0; j < n; j++) { // ... with n tiles in each line
             fout << randTile();
         }
         fout << endl; // end of line
     }
+
+    // Fout start (0,0) and end (n-1,n-1) coordinates
+    fout << "0 0" << endl;
+    fout << (n-1) << " " << (n-1) << endl;
+
+
     fout.close(); // Close file stream
     return 0;
 }
